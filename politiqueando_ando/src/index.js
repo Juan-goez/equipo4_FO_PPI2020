62429934893edadcc6407fa67940a672d814bcc9
cristian <cridamaca11@gmail.com>
2020-10-14 23:01:40 -0500

    Progreso subido

--- politiqueando_ando/src/index.js
+++ politiqueando_ando/src/index.js
@@ -1,17 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+//import Vida_Politica from './pages/vistas/Vida_Politica';
+import Aplicacion from './Aplicacion';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
+  <React.StrictMode>   
+     < Aplicacion/>
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
