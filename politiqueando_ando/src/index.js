9266790f936f19201c539cf770652cc9bf6d9913
cristian <cridamaca11@gmail.com>
2020-10-27 11:29:35 -0500

    x

--- politiqueando_ando/src/index.js
+++ politiqueando_ando/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import Vida_Politica from './pages/vistas/Vida_Politica';
+
 import Aplicacion from './Aplicacion';
 
 ReactDOM.render(
